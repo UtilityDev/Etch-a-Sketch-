5695e5938b77c119380012bc06cd1e0c2b7d019e
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-02-18 19:34:15 +0100

    Change grid size with button

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let grid = document.querySelector('.grid');
 
 const resizeBtn = document.querySelector('#resize-btn');
 
+const maxGridSize = 64;
+
+let cells = [];
+
 function createGrid(size) {
     for (let i = 0; i < size*size; i++) {
         let cell = document.createElement('div');
@@ -13,9 +17,24 @@ function createGrid(size) {
 }
 createGrid(16);
 
-let cells = document.querySelectorAll('.cell');
-cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = '#666666';
+function draw() {
+    cells = document.querySelectorAll('.cell')
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = '#666666';
+        });
     });
+}
+draw();
+
+// Resize grid
+resizeBtn.addEventListener('click', () => {
+    let size = parseInt(prompt('Grid Size (px): '));
+    if (size <= maxGridSize) {
+        cells.forEach((cell) => {
+            cell.remove();
+        });
+        createGrid(size);
+        draw();
+    }
 });
