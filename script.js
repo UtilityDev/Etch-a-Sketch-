945b64361b54d7e2fe3360e5ffc95128a853ea75
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-02-18 19:14:52 +0100

    Implement dynamic grid size!

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let grid = document.querySelector('.grid');
 
+const resizeBtn = document.querySelector('#resize-btn');
+
 function createGrid(size) {
     for (let i = 0; i < size*size; i++) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.style.width = `${grid.clientWidth/size}px`;
+        cell.style.height = `${grid.clientWidth/size}px`;
         grid.appendChild(cell);
     }
 }
